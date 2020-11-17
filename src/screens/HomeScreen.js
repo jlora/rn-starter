2919da4945e5ead85c8d5fa6bd3a4f0b496bf0a6
JESUS ANDRES LORA CAMACHO <andres@submarine.app>
2020-11-17 13:32:45 -0700

    First commit

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
+
+const HomeScreen = props => {
+  return <View>
+    <Text style={styles.text}>Home Screen sdfadf</Text>
+    <Button 
+      onPress={() => props.navigation.navigate('Components')}
+      title='Go to Components Demo'
+    />
+    <Button 
+      title='Go to List Demo'
+      onPress={() => props.navigation.navigate('List')}
+    />
+  </View>
+};
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 30
+  }
+});
+
+export default HomeScreen;
