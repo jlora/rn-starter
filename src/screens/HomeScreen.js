c247935576abf56cb92082dfcec53afb111a7e4b
JESUS ANDRES LORA CAMACHO <andres@submarine.app>
2020-11-18 11:34:46 -0700

    Created image and counter screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,16 +1,24 @@
 import React from "react";
 import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = props => {
+const HomeScreen = ({ navigation }) => {
   return <View>
     <Text style={styles.text}>Home Screen sdfadf</Text>
     <Button 
-      onPress={() => props.navigation.navigate('Components')}
+      onPress={() => navigation.navigate('Components')}
       title='Go to Components Demo'
     />
     <Button 
       title='Go to List Demo'
-      onPress={() => props.navigation.navigate('List')}
+      onPress={() => navigation.navigate('List')}
+    />
+    <Button 
+      title='Go to Image Demo'
+      onPress={() => navigation.navigate('Image')}
+    />
+    <Button 
+      title='Go to Counter Demo'
+      onPress={() => navigation.navigate('Counter')}
     />
   </View>
 };
