9bd734e5784250a179acad4f17e6f02bf40dfae0
JESUS ANDRES LORA CAMACHO <andres@submarine.app>
2020-11-20 17:39:35 -0700

    Added Text Screen with Primitive Component TextInput

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ import CounterScreen from './src/screens/CounterScreen';
 import CounterReducerScreen from './src/screens/CounterReducerScreen';
 import ColorScreen from './src/screens/ColorScreen';
 import SquareScreen from './src/screens/SquareScreen';
+import TextScreen from './src/screens/TextScreen';
 
 const navigator = createStackNavigator(
   {
@@ -19,6 +20,7 @@ const navigator = createStackNavigator(
     CounterReducer: CounterReducerScreen,
     Color: ColorScreen,
     Square: SquareScreen,
+    Text: TextScreen
   },
   {
     initialRouteName: 'Home',
